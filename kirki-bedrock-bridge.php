2e1faee0c05fb17a45359bc429bfa863b2589cea
TangRufus <tangrufus@gmail.com>
2020-04-08 14:54:58 +0100

    Intuition, however illogical, Mr. Spock, is recognized as a command prerogative.
    -- Captain Kirk

---
+++ kirki-bedrock-bridge.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Plugin Name:     Kirki Bedrock Bridge
+ * Plugin URI:      https://github.com/ItinerisLtd/kirki-bedrock-bridge/
+ * Description:     Make Kirki works on Bedrock.
+ * Version:         0.1.0
+ * Author:          Itineris Limited
+ * Author URI:      https://www.itineris.co.uk/
+ * Text Domain:     kirki-bedrock-bridge
+ */
+
+declare(strict_types=1);
+
+namespace Itineris\KirkiBedrockBridge;
+
+// If this file is called directly, abort.
+if (! defined('WPINC')) {
+    die;
+}
+
+add_filter('kirki/config', function ($config): array {
+    $config = (array) $config;
+
+    $config['url_path'] = plugins_url('/kirki/');
+
+    return $config;
+});
